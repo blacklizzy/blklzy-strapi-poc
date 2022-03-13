5e58a82b47751041d0144995015dd8a2b2385835
Robin Grönholm <robin.gronholm@icloud.com>
2022-03-13 14:08:30 +0100

    Trying to set up connection string

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,4 +1,5 @@
 const parse = require('pg-connection-string').parse;
+const DATABASE_URL = 'postgres://lsazatqalncvqc:e51462ecf9549ba6b2c975f3fcdb709ae5b61ebd24c5e8fd515ae3fd2864b048@ec2-54-171-25-232.eu-west-1.compute.amazonaws.com:5432/d45mt92dklic36';
 const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
